881fb7ddd4e5c2176781311b059c416b0b98087e
jamayapabon <jeap1723@gmail.com>
2024-08-01 19:17:39 -0500

    Fixed error in overwrite user, client and sale

--- query_template.py
+++ query_template.py
@@ -28,6 +28,11 @@ def create_user(body: dict):
         dict: A dictionary containing the HTTP status code and a message.
     """
     try:
+        user = db_consult(table_name=TABLE_USER, key_name="user_id", key_value=body["user_id"])
+        
+        if user:
+            return {"statusCode": 409, "body": f"user_id -{body['user_id']}- already exists"}
+       
         user_item = User(**body).model_dump()
 
         status = db_insert(table_name=TABLE_USER, item=user_item)
@@ -56,6 +61,11 @@ def create_client(body: dict):
         dict: A dictionary containing the HTTP status code and a message.
     """
     try:
+        client = db_consult(table_name=TABLE_CLIENT, key_name="client_id", key_value=body["client_id"])
+        
+        if client:
+            return {"statusCode": 409, "body": f"client_id -{body['client_id']}- already exists"}
+
         client_item = Client(**body).model_dump()
 
         status = db_insert(table_name=TABLE_CLIENT, item=client_item)
@@ -84,6 +94,11 @@ def create_sale(body: dict):
         dict: A dictionary containing the HTTP status code and a message.
     """
     try:
+        sale = db_consult(table_name=TABLE_SALE, key_name="sale_id", key_value=body["sale_id"])
+        
+        if sale:
+            return {"statusCode": 409, "body": f"sale_id -{body['sale_id']}- already exists"}
+        
         sale_item = Sale(**body).model_dump()
 
         status = db_insert(table_name=TABLE_SALE, item=sale_item)
