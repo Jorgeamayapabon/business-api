e8ea3642091c347737a168b42dd66600484face5
jamayapabon <jeap1723@gmail.com>
2024-08-01 19:08:58 -0500

    Fixed error in BaseModel of User, Client, Sale

--- base_models.py
+++ base_models.py
@@ -31,19 +31,49 @@ class HttpResponse(BaseModel):
 
 
 class User(BaseModel):
-    user_id: str
+    user_id: str | int
     fullname: str
     email: str
+    
+    def __init__(self, **data):
+        super().__init__(**data)
+        self.user_id = self._cast_str()
+    
+    def _cast_str(self):
+        if not isinstance(self.user_id, str):
+            return str(self.user_id)
+
+        return self.user_id
 
 
 class Client(BaseModel):
-    client_id: str
+    client_id: str | int
     name: str
     email: str
+    
+    def __init__(self, **data):
+        super().__init__(**data)
+        self.client_id = self._cast_str()
+    
+    def _cast_str(self):
+        if not isinstance(self.client_id, str):
+            return str(self.client_id)
+
+        return self.client_id
 
 
 class Sale(BaseModel):
-    sale_id: str
+    sale_id: str | int
     user_id: str
     client_id: str
     value: float
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        self.sale_id = self._cast_str()
+    
+    def _cast_str(self):
+        if not isinstance(self.sale_id, str):
+            return str(self.sale_id)
+
+        return self.sale_id
